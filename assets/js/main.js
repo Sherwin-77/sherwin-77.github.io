706327244755e96b1c4a2b0e1a2c1d474a7338f2
Sherwin-77 <sherwinak1@gmail.com>
2023-12-08 11:30:17 +0700

    Moving game handler to its own object

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,65 +1,101 @@
-$(document).ready(function () {
-  const $mainField = $("#main-field");
-  const $scores = $("#scores");
-  const $circle = $("#circle");
-  let score = 0;
-  let idx = 1;
-  function doIncrement(value) {
-    $({ scoreValue: score }).animate(
-      { scoreValue: score + value },
+const gameSystem = {
+  $mainField: null,
+  $scores: null,
+  $circle: null,
+  key1: "z",
+  key2: "x",
+  idx: 1,
+  score: 0,
+  x: -1,
+  y: -1,
+  _keys: {},
+  init: function () {
+    gameSystem.$mainField = $("#main-field");
+    gameSystem.$scores = $("#scores");
+    gameSystem.$circle = $("#circle");
+    $(document).mousemove(function(e) {
+      gameSystem.x = e.pageX;
+      gameSystem.y = e.pageY;
+  });
+    $(document).on("keydown", function (e) {
+      const ch = String.fromCharCode(e.which).toLowerCase();
+      if (ch !== gameSystem.key1 && ch !== gameSystem.key2) return;
+      if (gameSystem._keys[ch]) return;
+      gameSystem._keys[ch] = true;
+      document.elementFromPoint(gameSystem.x - window.pageXOffset, gameSystem.y - window.pageYOffset)?.click();
+    });
+    $(document).on("keyup", function (e) {
+      const ch = String.fromCharCode(e.which).toLowerCase();
+      if (ch !== gameSystem.key1 && ch !== gameSystem.key2) return;
+      gameSystem._keys[ch] = false;
+    });
+    gameSystem.$circle.click(function (e) {
+      gameSystem.idx++;
+      $("#circle-number").text(gameSystem.idx);
+      gameSystem.doIncrement(300);
+      const oldLeft = gameSystem.$circle.css("left");
+      const oldTop = gameSystem.$circle.css("top");
+      $("#circle-effect")
+        .clone()
+        .css({
+          display: "block",
+          left: oldLeft,
+          top: oldTop,
+        })
+        .appendTo("#main-field")
+        .fadeOut(function () {
+          $(this).remove();
+        });
+      let newLeft =
+        Math.random() *
+          (gameSystem.$mainField.width() - gameSystem.$circle.width() - 5) +
+        5;
+      let newTop =
+        Math.random() *
+          (gameSystem.$mainField.height() - gameSystem.$circle.height() - 5) +
+        5;
+      if (Math.abs(oldLeft - newLeft) < 50)
+        newLeft = Math.max(
+          (newLeft * 2) %
+            (gameSystem.$mainField.width() - gameSystem.$circle.width()),
+          10
+        );
+      if (Math.abs(oldTop - newTop) < 50)
+        newTop = Math.max(
+          (newTop * 2) %
+            (gameSystem.$mainField.height() - gameSystem.$circle.height()),
+          10
+        );
+      $(this).css({
+        left: newLeft,
+        top: newTop,
+      });
+    });
+  },
+  doIncrement: function (value) {
+    $({ scoreValue: gameSystem.score }).animate(
+      { scoreValue: gameSystem.score + value },
       {
         duration: 300,
         easing: "swing",
         step: function () {
-          $scores.text(Math.ceil(this.scoreValue));
+          gameSystem.$scores.text(Math.ceil(this.scoreValue));
         },
         complete: function () {
-          score += value;
-          $scores.text(score);
+          gameSystem.score += value;
+          gameSystem.$scores.text(gameSystem.score);
         },
       }
     );
-  }
+  },
+};
+
+$(document).ready(function () {
   $(".anm").viewportChecker({
     classToAdd: "visible",
     offset: -50,
     classToRemove: "anm",
     repeat: false,
   });
-  $circle.click(function (e) {
-    idx++;
-    $("#circle-number").text(idx);
-    doIncrement(300);
-    const oldLeft = $circle.css("left");
-    const oldTop = $circle.css("top");
-    $("#circle-effect")
-      .clone()
-      .css({
-        display: "block",
-        left: oldLeft,
-        top: oldTop,
-      })
-      .appendTo("#main-field")
-      .fadeOut(function () {
-        $(this).remove();
-      });
-    let newLeft =
-      Math.random() * ($mainField.width() - $circle.width() - 5) + 5;
-    let newTop =
-      Math.random() * ($mainField.height() - $circle.height() - 5) + 5;
-    if (Math.abs(oldLeft - newLeft) < 50)
-      newLeft = Math.max(
-        (newLeft * 2) % ($mainField.width() - $circle.width()),
-        10
-      );
-    if (Math.abs(oldTop - newTop) < 50)
-      newTop = Math.max(
-        (newTop * 2) % ($mainField.height() - $circle.height()),
-        10
-      );
-    $(this).css({
-      left: newLeft,
-      top: newTop,
-    });
-  });
+  gameSystem.init();
 });
